39a078d30c23791efa0fce9094cfb8bcc7ba0e2a
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-28 10:32:21 +0200

    Finish implementation of enumerable methods

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -2,13 +2,18 @@ module MyEnumerable
   def all?(&block)
     all_return = @list.map(&block)
     !all_return.include? false
-end
+  end
 
-def any?(&block)
-  any_return = @list.map(&block)
-  any_return.include? true
-end
+  def any?(&block)
+    any_return = @list.map(&block)
+    any_return.include? true
+  end
 
-def filter 
-  filter = []
-  @list.each do |e|
+  def filter 
+    filter = []
+    @list.each do |e|
+      filter.push(e) if yield(e)
+    end
+    filter
+  end
+end
